5b2e645c8e5a531ee932b5a8b192b2e7306c8a2f
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2021-09-22 16:23:39 +0900

    algorithm for Bing image crawling

---
+++ index.py
@@ -0,0 +1,49 @@
+from bing_image_downloader import downloader
+
+downloader.download("워너원 강다니엘", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+
+downloader.download("엑소 백현", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+
+downloader.download("박보검", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+
+downloader.download("송중기", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("워너원 황민현", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("엑소 시우민", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("강동원", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("이종석", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("이준기", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("마동석", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("조진웅", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("조세호", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("안재홍", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("윤두준", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("이민기", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("김우빈", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("육성재", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("공유", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("방탄소년단 정국", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("아이콘 바비", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("워너원 박지훈", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
+downloader.download("엑소 수호", limit=50,  output_dir='dataset', 
+                    adult_filter_off=True, force_replace=False, timeout=60)
